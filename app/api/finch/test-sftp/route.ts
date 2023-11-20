3bb13ffc9a4d9e30dceb8652ce2f4b1d1e8602f6
Tyler Nix <github@cometcopy.com>
2023-11-20 11:15:13 -0600

    test sftp route

---
+++ app/api/finch/test-sftp/route.ts
@@ -0,0 +1,46 @@
+import { NextResponse } from "next/server";
+import { createSFTPClient } from '@/utils/sftp';
+import moment from "moment";
+
+const sftpClient = createSFTPClient()
+const sftpPath = process.env.SFTP_PATH
+const testSftpKey = process.env.TEST_SFTP_KEY
+
+export async function POST(req: Request) {
+    console.log(req.method + " /api/finch/test-sftp");
+
+    const { test_sftp_key } = await req.json()
+    const csv = `
+    ID,Name,Department
+    1,John Doe,Finance
+    2,Jane Smith,Marketing
+    3,Emily Johnson,Human Resources
+    4,Michael Brown,IT
+    `
+
+    try {
+        if (test_sftp_key == testSftpKey) {
+            console.log("Sending CSV file to SFTP")
+            await sftpClient.putCSV(csv, `${sftpPath}/finch-test-${moment().format('YYYY-MM-DD')}.csv`);
+            console.log('File uploaded via SFTP successfully');
+
+            return new NextResponse(
+                JSON.stringify({ ok: true })
+            )
+
+        }
+
+
+    } catch (error) {
+        console.error('An error occurred:', error);
+        return new NextResponse(
+            JSON.stringify({ ok: false })
+        )
+
+    }
+
+
+
+
+
+}
